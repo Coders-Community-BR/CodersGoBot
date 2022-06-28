72e7cce6c56d699c033a27503f582800cf7dcdcd
Nik <yxqsnz@gmail.com>
2022-06-28 17:49:51 -0300

    feat(commandhandler): add ReplyEmbed function (#20)

--- commandHandler/context.go
+++ commandHandler/context.go
@@ -16,3 +16,13 @@ func (context *Context) ReplyText(content string) error {
 	})
 	return err
 }
+
+func (context *Context) ReplyEmbed(embed *discordgo.MessageEmbed) error {
+	err := context.Session.InteractionRespond(context.Event.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{embed},
+		},
+	})
+	return err
+}
