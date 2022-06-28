cd2d3a928ca0b4be85bb8decb2fda3a24f994367
Nik <yxqsnz@gmail.com>
2022-06-28 19:25:27 -0300

    Server info command and add Discord Utils (#21)
    * feat(util): add utils for discord
    
    * feat(main): enable state and loglevel
    
    * feat(commands): add /server command

---
+++ util/discord.go
@@ -0,0 +1,31 @@
+package util
+
+import (
+	"fmt"
+
+	"github.com/Coders-Community-BR/CodersGoBot/commandHandler"
+	"github.com/bwmarrin/discordgo"
+)
+
+func CodeBlock(code interface{}) string {
+	return fmt.Sprintf("`%s`", code)
+}
+
+func GetUserNameByContext(context *commandHandler.Context) string {
+	member := context.Event.Member
+	user := context.Event.Member.User
+	if member.Nick != "" {
+		return member.Nick
+	} else {
+		return user.Username
+	}
+}
+
+func GetStateGuild(session *discordgo.Session, guildID string) *discordgo.Guild {
+	for _, guild := range session.State.Guilds {
+		if guild.ID == guildID {
+			return guild
+		}
+	}
+	return nil
+}
