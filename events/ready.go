ca07b468aa973626c8a50b3c5734639986110089
Nik <yxqsnz@gmail.com>
2022-07-02 11:36:45 -0300

    feat: add status member count (#30)
    * fix: now status change in 20 seconds. fixes #28
    
    * fix: re-add `ready` event handler
    
    * feat: add member count to status
    
    * chore: change status update time to 5

--- events/ready.go
+++ events/ready.go
@@ -1,6 +1,7 @@
 package events
 
 import (
+	"fmt"
 	"time"
 
 	_ "github.com/Coders-Community-BR/CodersGoBot/commands/info"
@@ -20,8 +21,10 @@ func Ready(s *discordgo.Session, m *discordgo.Ready) {
 	go func(s *discordgo.Session) {
 		for {
 			s.UpdateGameStatus(0, guild.Name)
-			time.Sleep(time.Duration(20 * time.Second))
-			s.UpdateGameStatus(0, "0 membros")
+			time.Sleep(time.Duration(5 * time.Second))
+			if guild := util.GetStateGuild(s, util.GetEnv("GUILD_ID")); guild != nil {
+				s.UpdateListeningStatus(fmt.Sprintf("%d membros", guild.MemberCount))
+			}
 		}
 	}(s)
 }
